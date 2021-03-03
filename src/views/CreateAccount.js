3b96802b576080e3eea1733b9d13c8a997ecae15
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-03 16:46:18 -0500

    created new user account form

--- src/views/CreateAccount.js
+++ src/views/CreateAccount.js
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const URL = "http://localhost:5000"
+
+const initialForm = {
+  type: '',
+  name: '',
+  username: '',
+  password: '',
+}
 
 function CreateAccount() {
+  const [formValues, setFormValues] = useState(initialForm);
+
+  const onChange = evt => {
+    const { name, value } = evt.target;
+
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+
+    const newUser = {
+      username: formValues.username,
+      password: formValues.password,
+      name: formValues.name,
+    }
+
+    console.log(newUser);
+  }
+
   return (
-    <div>
-      <h1>CreateAccount</h1>
-    </div>
+    <>
+      <h2>Create Account</h2>
+      <form onSubmit={onSubmit}>
+        <label for="type">
+          Account Type:
+          <select 
+            name="type" 
+            onChange={onChange}
+            value={formValues.type}
+          >
+            <option value="">---Who are you?---</option>
+            <option value="parent">Parent</option>
+            <option value="child">Child</option>
+          </select>
+        </label>
+        <label for="name">
+          Name:
+          <input 
+            type="text" 
+            name="name"
+            onChange={onChange}
+            value={formValues.name}
+          />
+        </label>
+        <label for="username">
+          User Name:
+          <input 
+            type="text" 
+            name="username"
+            onChange={onChange}
+            value={formValues.username}
+          />
+        </label>
+        <label for="password">
+          Password:
+          <input 
+            type="text" 
+            name="password"
+            onChange={onChange}
+            value={formValues.password}
+          />
+        </label>
+        <button>Sign Up</button>
+      </form>
+    </>
   )
 }
 
