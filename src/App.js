948b83bcf567b170546442bce8dfe9463af76ad9
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-03 16:12:08 -0500

    installed routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+
 import Header from './components/Header'
 import AccountNav from './components/AccountNav'
 
 import Home from './views/Home'
+import Register from './views/CreateAccount'
 
 import './App.css';
 
@@ -10,7 +14,14 @@ function App() {
     <div className="App">
       <Header />
       <AccountNav />
-      <Home />
+      <Switch>
+        <Route path="/account/create">
+          <Register/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,25 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-import AccountNav from './AccountNav'
-
+const AppHeader = styled.header`
+  background-color: #FFB113;
+  min-height: 5vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  box-shadow: 0px 1px 2px gray;
+  position: relative;
+`;
 
 function Header() {
-  const AppHeader = styled.header`
-    background-color: #FFB113;
-    min-height: 5vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-    box-shadow: 0px 1px 2px gray;
-    position: relative;
-  `;
   return (
     
     <AppHeader>
       
     </AppHeader>
-      
-      
     
   )
 }
